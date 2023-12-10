812aa26b22689b0de62d3e20fa9429d2d3662678
Michel-Takla <michelusa497@gmail.com>
2023-12-10 02:28:47 -0500

    create loop

--- script.js
+++ script.js
@@ -2,10 +2,17 @@
 // 2- lower case 
 // 3- numbers
 // 4- special characters
-// 
+// 5- the user choice
+// 6- the input for the password
+// 7- generate the password using loop x count
+// 8- final, showing the password to the page
 
-let upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-let lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+let upperCase = "ABCDEFGHIJKLMNOPQRSTUVUXYZ";
+let lowerCase = "abcdefghijklmnopqrstuvwxyz";
+let spcialCharacters = "!@#$%^&*()_-+=[]{}|?<>~"
+let numbers = "1234567890"
+let charsCount = 8;
+let choice = "";
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
@@ -19,6 +26,13 @@ function writePassword() {
   passwordText.value = Password;
 
 }
+
+function generatePassword () {
+  // Here is to generate the password using loop
+  for (let i = 0; i < charsCount; i++) {
+    choice = Math.floor(Math.random () * choice.length);
+  }
+} 
   
-  // Add event listener to generate button
+// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
